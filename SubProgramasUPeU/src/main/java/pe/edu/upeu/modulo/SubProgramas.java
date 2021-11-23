6d9cea9486544133363cd63ba802b5f737d99ebb
jhanslarico1 <91982256+jhanslarico1@users.noreply.github.com>
2021-11-23 16:40:26 +0000

    SubProgramas

---
+++ SubProgramasUPeU/src/main/java/pe/edu/upeu/modulo/SubProgramas.java
@@ -0,0 +1,19 @@
+package pe.edu.upeu.modulo;
+
+
+public class SubProgramas{
+  
+  public String nombre="Dario";
+  private String dni="";
+
+
+
+  public void saludo(){
+    System.out.println("Hola "+nombre);
+  }
+
+  public void otroSaludo(){    
+    String apellidos="Perez";
+    System.out.println("Hola "+nombre+" "+apellidos);
+  }
+}
